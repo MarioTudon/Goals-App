5d9cbfa6c5eae927ab1b7e60ec78d881e81f835b
Mario Tudón <mario.tudon97@gmail.com>
2024-09-05 09:23:12 -0600

    Se modifico la ruta root de vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: resolve(__dirname,'src'),
   base:'',
   plugins: [react(),createHtmlPlugin({minify:true})]
 })
