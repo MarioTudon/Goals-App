9288d5ede053e2a1896b346e0e00d9d2960cb8b1
Mario Tudón <mario.tudon97@gmail.com>
2024-09-05 09:32:18 -0600

    Se modifico base en vite.config.js para ser mas explicita

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { createHtmlPlugin } from 'vite-plugin-html'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'',
+  base:'/Goals-App/',
   plugins: [react(),createHtmlPlugin({minify:true})]
 })
