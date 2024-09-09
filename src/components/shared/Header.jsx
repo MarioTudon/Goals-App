c83a4439509efbfc0b046361b6b9c0126285eacc
Mario Tudón <mario.tudon97@gmail.com>
2024-09-09 16:48:23 -0600

    Cambios menores

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import logo from '../../assets/logo.svg'
 import account from '../../assets/account.svg'
 
@@ -8,12 +7,12 @@ function Header() {
         <>
             <div className='h-xl flex justify-between shadow bg-white text-gray-700'>
                 <div className='flex items-center pl-4'>
-                    <img src={logo} alt="" className='mr-2 w-11 h-11' />
+                    <img src={logo} alt="logo" className='mr-2 w-11 h-11' />
                     <a href="/" className='text-2xl font-bold'>Goals App</a>
                 </div>
                 <nav className='flex items-center pr-4'>
                     <a href="/perfil">
-                        <img src={account} alt="" className='mr-2 w-10 h-10'/>
+                        <img src={account} alt="profile" className='mr-2 w-10 h-10'/>
                     </a>
                 </nav>
             </div>
