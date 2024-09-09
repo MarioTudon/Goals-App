7dc9a5fbfd461e95a596e333cbf70c2fbdf8fcc8
Mario Tudón <mario.tudon97@gmail.com>
2024-09-09 10:42:01 -0600

    Se importaron componentes en App.jsx y se implementaron

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/shared/header'
+import Main from './components/shared/main'
+import Footer from './components/shared/Footer'
 
 function App() {
 
   return (
     <>
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
