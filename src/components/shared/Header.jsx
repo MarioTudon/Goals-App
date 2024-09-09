05ac1ddc44dec2131b5728837375942e53e1c236
Mario Tudón <mario.tudon97@gmail.com>
2024-09-09 15:09:31 -0600

    Se definio el header (estilo y layout)

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -8,8 +8,8 @@ function Header() {
         <>
             <div className='h-xl flex justify-between shadow bg-white text-gray-700'>
                 <div className='flex items-center pl-4'>
-                    <img src={logo} alt="" className='mr-2 w-10 h-10' />
-                    <a href="/">Goals App</a>
+                    <img src={logo} alt="" className='mr-2 w-11 h-11' />
+                    <a href="/" className='text-2xl font-bold'>Goals App</a>
                 </div>
                 <nav className='flex items-center pr-4'>
                     <a href="/perfil">
