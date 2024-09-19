c1f222fd13401221129809e35a7325860de8f070
Mario Tudón <mario.tudon97@gmail.com>
2024-09-19 15:45:08 -0600

    Cambios realizados

--- src/components/shared/LinkButton.jsx
+++ src/components/shared/LinkButton.jsx
@@ -1,9 +1,9 @@
 function LinkButton({ children, text, href, imgSrc, imgAlt }) {
     return (
         <>
-            <a href={href} className='flex'>
-                <img src={imgSrc} alt={imgAlt} />
-                {text}
+            <a href={href} className='flex pt-4 items-center'>
+                <img src={imgSrc} alt={imgAlt} className="w-6 h-6 ml-4 mr-3"/>
+                <span className="align-middle">{text}</span>
             </a>
         </>
     );
--- src/components/shared/Main.jsx
+++ src/components/shared/Main.jsx
@@ -1,14 +1,14 @@
 import LinkButton from './LinkButton'
-import addCircle from '../../assets/addCircle.svg'
+import addGoal from '../../assets/add-goal.svg'
 import checklist from '../../assets/checklist.svg'
 
 function Main({ children }) {
     return (
         <>
-            <div className='h-main flex'>
+            <div className='h-main flex items-center'>
                 <aside className='flex flex-col text-gray-700 bg-gray-100 shadow w-60 h-full'>
-                    <LinkButton href={'/list'} text={'List'} imgSrc={checklist} imgAlt={'checklist'}/>
-                    <LinkButton href={'/list'} text={'List'} imgSrc={addCircle} imgAlt={'addCircle'}/>
+                    <LinkButton href={'/list'} text={'Goals List'} imgSrc={checklist} imgAlt={'checklist'}/>
+                    <LinkButton href={'/list'} text={'New Goal'} imgSrc={addGoal} imgAlt={'addCircle'}/>
                 </aside>
                 <main className='w-full h-full overflow-scroll'>
                     {children}
