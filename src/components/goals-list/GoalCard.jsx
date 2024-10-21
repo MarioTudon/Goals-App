f51d215a29f9b3f006eb68dffa71003d1d038ede
Mario Tudón <mario.tudon97@gmail.com>
2024-10-21 09:48:09 -0600

    Se modifico estilo de tarjetas

--- src/components/goals-list/GoalCard.jsx
+++ src/components/goals-list/GoalCard.jsx
@@ -29,16 +29,16 @@ function GoalCard({ icon, goal, frequency, frequencyUnit, initialCount, initialT
         <>
             <div className="flex justify-between items-center w-full h-auto bg-gray-200 p-2 rounded-xl shadow-md shadow-gray-400" onClick={handleClick}>
                 <div className="flex">
-                    <div className="w-11 h-11 flex justify-center items-center bg-gray-50 rounded-full shadow-sm shadow-gray-400">{icon}</div>
+                    <div className="w-14 h-14 flex text-2xl justify-center items-center bg-gray-50 rounded-full shadow-inner shadow-gray-400">{icon}</div>
                 </div>
                 <div className="flex flex-col items-center text-sm font-semibold text-center w-24 mx-1 h-auto">
                     <p className="font-bold overflow-x-auto w-full">{goal}</p>
                     <p>{frequency}<sub>/{frequencyUnit}</sub></p>
                 </div>
                 <div className="flex items-center">
-                    <div className="flex flex-col items-center mr-2 w-16">
+                    <div className="flex flex-col items-center mr-2 w-20">
                         <div className="text-sm font-semibold text-center">{count} of {target}</div>
-                        <div className="w-full h-2 rounded-full bg-gray-300 relative">
+                        <div className="w-full h-2 rounded-full bg-gray-300 relative  shadow-sm shadow-gray-400">
                             <div className={"h-2 rounded-full bg-gradient-to-r from-emerald-300 to-cyan-500 absolute left-0"} style={{ 'width': `${percentage}%` }}></div>
                         </div>
                     </div>
