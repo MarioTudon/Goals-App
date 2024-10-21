d243921a576d585013988dfadb1af117fb94ce78
Mario Tudón <mario.tudon97@gmail.com>
2024-10-21 09:41:45 -0600

    Se movio boton de menu lateral

--- src/components/main/SideMenu.jsx
+++ src/components/main/SideMenu.jsx
@@ -49,7 +49,7 @@ function SideMenu({ sendDataToMain }) {
                         <path d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7" strokeWidth="1.5" strokeLinecap="round" className='stroke-gray-700' />
                     </svg>
                 </SideMenuButton>
-                <button onClick={hideMenu} className='absolute top-3/4 right-0 bg-gray-100 translate-x-full w-4 h-16 flex items-center justify-center bg-gray rounded-r-full transition-all duration-medium z-20'>
+                <button onClick={hideMenu} className='absolute top-1/2 right-0 bg-gray-100 translate-x-full w-4 h-16 flex items-center justify-center bg-gray rounded-r-full transition-all duration-medium z-20'>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#e8eaed" className={`fill-gray-700 ml-auto min-w-6 min-h-6 ${arrowRotation} transition-all duration-medium animate-${arrowDirection} relative`} >
                         <path d="M490-480 342-628q-18-18-18-41.5t18-41.5q18-18 41.5-18t41.84 18.34L614-522q8 8.4 13 19.7 5 11.3 5 22.5t-5 22.5q-5 11.3-13 19.3L425.34-249.34Q407-231 384-231.5q-23-.5-41-18.5t-18-41.5q0-23.5 18-41.5l147-147Z" />
                     </svg>
